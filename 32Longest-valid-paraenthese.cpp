da436f3c15e9ace47fab1b939952bf3f85526fdc
GOPESH RATHINAM <gopeshrathnam_bcs28@mepcoeng.ac.in>
2026-08-11 16:40:49 +0530

    Add solution for longest valid parentheses
    Implemented a solution to find the length of the longest valid parentheses substring using a two-pass approach.

---
+++ 32Longest-valid-paraenthese.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int left = 0, right = 0, maxLen = 0;
+        int n = s.length();
+        
+        // Left-to-right pass
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '(') {
+                left++;
+            } else {
+                right++;
+            }
+            
+            if (left == right) {
+                maxLen = max(maxLen, 2 * right);
+            } else if (right > left) {
+                left = right = 0;
+            }
+        }
+        
+        left = right = 0;
+        
+        // Right-to-left pass
+        for (int i = n - 1; i >= 0; i--) {
+            if (s[i] == '(') {
+                left++;
+            } else {
+                right++;
+            }
+            
+            if (left == right) {
+                maxLen = max(maxLen, 2 * left);
+            } else if (left > right) {
+                left = right = 0;
+            }
+        }
+        
+        return maxLen;
+    }
+};
